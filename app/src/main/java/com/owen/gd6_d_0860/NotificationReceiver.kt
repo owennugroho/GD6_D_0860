1ff5544da97a015a291aed7ec7f7f31a849b13c9
owennugroho <owennugroho15@gmail.com>
2022-09-26 18:31:19 +0700

    Initial commit

---
+++ app/src/main/java/com/owen/gd6_d_0860/NotificationReceiver.kt
@@ -0,0 +1,13 @@
+package com.owen.gd6_d_0860
+
+import android.content.BroadcastReceiver
+import android.content.Context
+import android.content.Intent
+import android.widget.Toast
+
+class NotificationReceiver : BroadcastReceiver(){
+    override fun onReceive(context: Context?, intent: Intent) {
+        val message = intent.getStringExtra("toastMessage")
+        Toast.makeText(context, message, Toast.LENGTH_LONG).show()
+    }
+}
